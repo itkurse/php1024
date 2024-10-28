5bcc287d16b28bf57fad5ae197b9eab74a3e2984
itkurse <89607257+itkurse@users.noreply.github.com>
2024-10-28 08:51:48 +0100

    DDL und Klasse DbAccess

---
+++ abteilung-person/db/dbaccess.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+class DbAccess 
+{
+    // PDO-Connection ist die Verbindung zur Datenbank
+    private PDO $conn;
+
+    // Konstruktor, wird aufgerufen wenn ein neues Objekt der Klasse
+    // DbAccess erzeugt wird
+    public function __construct(){
+        
+    }
+}
+
+?>
---
+++ abteilung-person/db/ddl.sql
@@ -0,0 +1,24 @@
+-- DDL: Data Definition Language
+
+CREATE DATABASE 20241028_abteilungpersonen;
+USE 20241028_abteilungpersonen;
+
+CREATE TABLE abteilung 
+(
+    id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE person 
+(
+    id INT AUTO_INCREMENT,
+    vorname VARCHAR(100) NOT NULL,
+    nachname VARCHAR(100) NOT NULL,
+    geburtsdatum DATE NOT NULL,
+    gehalt DECIMAL(10, 2) NOT NULL,
+    abteilung_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (abteilung_id) REFERENCES abteilung(id)
+);
